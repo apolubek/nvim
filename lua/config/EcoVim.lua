e6d46fd11c1b6db3c12262831a370d3f5edbd6f9
Adam Połubek <adam.polubek@tooploox.com>
2025-12-01 14:39:20 +0100

    Support monorepo livegrep

--- lua/config/EcoVim.lua
+++ lua/config/EcoVim.lua
@@ -40,7 +40,7 @@ EcoVim = {
     rooter = {
       -- Removing package.json from list in Monorepo Frontend Project can be helpful
       -- By that your live_grep will work related to whole project, not specific package
-      patterns = { ".git", "package.json", "_darcs", ".bzr", ".svn", "Makefile" }, -- Default
+      patterns = { ".git", "_darcs", ".bzr", ".svn", "Makefile" }, -- Default
     },
   },
   -- Please keep it
